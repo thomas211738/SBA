a608889cfc9270135f225be4ce706ff526fdbe6e
Thomas Boles <thomas211738@gmail.com>
2024-03-26 20:39:29 -0400

    c

--- scraping.py
+++ scraping.py
@@ -1,11 +1,10 @@
 
 import requests
 
-API_KEY = 'eeef357467bd0f55cf650632a5ceab04'
-sport = 'icehockey_nhl'
-url = f'https://api.the-odds-api.com/v4/sports/{sport}/odds/?apiKey={API_KEY}&regions=us&markets=h2h,spreads&oddsFormat=american'
+# sport = 'icehockey_nhl'
+# url = f'https://api.the-odds-api.com/v4/sports/{sport}/odds/?apiKey={API_KEY}&regions=us&markets=h2h,spreads&oddsFormat=american'
 
-response = requests.get(url)
-data = response.json()
+# response = requests.get(url)
+# data = response.json()
 
-print(data)
+# print(data)
